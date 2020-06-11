2a10f57c5051a6d2ac4c9f31eaba2eefb98bb7b9
김용연 SKCC <jobkim0410@partner.sk.com>
2020-06-11 17:34:43 +0900

    eject add .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 import BoardList from "./BoardList";
 import BoardDetail from "./BoardDetail";
+import Counter from "./Counter";
+import CounterDeco from "./CounterDeco";
 
 function App() {
   return (
@@ -15,11 +17,19 @@ function App() {
                       <li>
                           <Link to="/boardDetail">글쓰기</Link>
                       </li>
+                      <li>
+                          <Link to="/counter">카운터</Link>
+                      </li>
+                      <li>
+                          <Link to="/counterDeco">카운터</Link>
+                      </li>
                   </ul>
               </nav>
 
               <Route exact path='/' component={BoardList}/>
               <Route path='/boardDetail' component={BoardDetail}/>
+              <Route path='/counter' component={Counter}/>
+              <Route path='/counterDeco' component={CounterDeco}/>
           </div>
       </Router>
   );
